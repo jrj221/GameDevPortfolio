051589a714db4884da1a012f44e6988704d4cb52
Jack Johnson <jrj221@byu.edu>
2026-02-27 18:40:47 -0700

    Edited Contact.tsx to make a fetch request to a Google Apps Script that sends an email with the contact form information to me upon submission

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,10 +1,38 @@
 import "./Portfolio.css";
 
 const Contact = () => {
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault(); // Stop page refresh
+		const form = e.currentTarget;
+
+		const formData = new FormData(form);
+
+		const data = {
+			name: formData.get("name"),
+			email: formData.get("email"),
+			message: formData.get("message"),
+		};
+
+		try {
+			await fetch(
+				"https://script.google.com/macros/s/AKfycbxMmUvKrrXq-mXgDPkpCEHeGUslF247HKCpiifTX-841OgvXr2fm_17kffDojPYF-xM/exec",
+				{
+					method: "POST",
+					mode: "no-cors",
+					body: JSON.stringify(data),
+				}
+			);
+
+			form.reset(); // Clears the form
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
 	return (
 		<section className="section container" id="contact">
 			<h2 className="section-title">Contact</h2>
-			<form className="contact-form" action="#" method="post">
+			<form className="contact-form" action="#" method="post" onSubmit={handleSubmit}>
 				<input className="form-input" type="text" name="name" placeholder="Your name" required />
 				<input className="form-input" type="email" name="email" placeholder="Email address" required />
 				<textarea className="form-input" name="message" placeholder="Your message" required></textarea>
